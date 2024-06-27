2a1b81b0c6b563cdbe598b9535a16ddda266b1a9
ChrisMG <chrismg.datanalysis@gmail.com>
2024-06-27 13:34:18 -0600

    updating myscript

--- my_script.py
+++ my_script.py
@@ -1,2 +1,9 @@
 import os
 print(f'My working directory is {os.getcwd()}')
+
+print("I don't know python")
+x = 40
+if x % 2 == 0:
+    print(f"{x} is a fun number")
+else:
+    print(f"{x} is a weird number")
